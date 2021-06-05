eb1e70ba91031e5ec5fe4a6c640421838dd55cc1
Saba Khutsishvili <sabakhuts@gmail.com>
2021-06-05 08:45:00 +0400

    Add solution to 'Police Chase' problem

--- problems/police-chase/police-chase/main.cpp
+++ problems/police-chase/police-chase/main.cpp
@@ -17,7 +17,21 @@ using namespace std;
 const ll N = 501, INF = 1e9;
 ll n, m;
 vector<vector<ll>> adj;
+vector<vector<ll>> orig(N, vector<ll> (N));
 vector<vector<ll>> cap(N, vector<ll> (N));
+vector<bool> vis(N);
+vector<pair<ll, ll>> ans;
+
+void resize() {
+    vis.resize(n+1);
+    adj.resize(n+1);
+    cap.resize(n+1);
+    orig.resize(n+1);
+    for (ll i = 0; i <= n; ++i) {
+        cap[i].resize(n+1);
+        orig[i].resize(n+1);
+    }
+}
 
 ll bfs(ll s, ll t, vector<ll> &par) {
     fill(par.begin(), par.end(), -1);
@@ -41,7 +55,7 @@ ll bfs(ll s, ll t, vector<ll> &par) {
     return 0;
 }
 
-void mincut(ll s, ll t) {
+void maxflow(ll s, ll t) {
     vector<ll> par(n+1);
     ll ans = 0, flow;
     while ((flow = bfs(s, t, par))) {
@@ -56,24 +70,47 @@ void mincut(ll s, ll t) {
     }
 }
 
-void print_path() {
-    cout << "\n";
+void dfs(ll i) {
+    vis[i] = 1;
+    for (ll a : adj[i]) {
+        if (!vis[a] && cap[i][a]) {
+            dfs(a);
+        }
+    }
+}
+
+void mincut() {
+    dfs(1);
+    for (ll i = 1; i <= n; ++i) {
+        for (ll j = 1; j <= n; ++j) {
+            if (vis[i] && !vis[j]) { // i reachable, j not
+                if (orig[i][j]) {
+                    ans.push_back({i, j});
+                }
+            }
+        }
+    }
+    ll sz = (ll)ans.size();
+    cout << sz << "\n";
+    for (ll i = 0; i < sz; ++i) {
+        cout << ans[i].first << " " << ans[i].second << "\n";
+    }
 }
 
 int main() {
     ll a, b;
     
     cin >> n >> m;
-    adj.resize(n+1);
+    resize();
     for (ll i = 0; i < m; ++i) {
         cin >> a >> b;
         adj[a].push_back(b);
         adj[b].push_back(a);
-        cap[a][b] = 1;
-        cap[b][a] = 1;
+        cap[a][b] = orig[a][b] = 1;
+        cap[b][a] = orig[b][a] = 1;
     }
-    mincut(1, n);
-    print_path();
+    maxflow(1, n);
+    mincut();
     
     return 0;
 }
